24e35e575165bd9f660ed91a8baf18425fac29ce
DonH-MobileApps <dharris.csu@yahoo.com>
2019-02-15 12:59:51 -0500

    Problem 5

---
+++ app/src/main/java/com/example/wk1test/MainActivity.java
@@ -0,0 +1,58 @@
+package com.example.wk1test;
+
+
+// problem 5 source code
+
+public class MainActivity{
+
+
+
+        public static void main(String[] args) {
+
+            int tableSize = 10;
+
+            printMultiplicationTable(tableSize);
+
+        }
+
+
+
+        private static void printMultiplicationTable(int tableSize) {
+
+            // first print the top header row
+
+            System.out.format("      ");
+
+            for(int i = 1; i<=tableSize;i++ ) {
+
+                System.out.format("%4d",i);
+
+            }
+
+            System.out.println();
+
+            System.out.println("      ----------------------------------------");
+
+
+
+            for(int i = 0 ;i<=tableSize;i++) {
+
+                // print left most column first
+
+                System.out.format("%4d |",i);
+
+                for(int j=1;j<=tableSize;j++) {
+
+                    System.out.format("%4d",i*j);
+
+                }
+                 System.out.println();
+
+            }
+
+        }
+
+
+
+    }
+
